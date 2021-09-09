9500ef202b6abbe1778bc9e20303395dc8fe7da2
Angelo <ming.bu@weizhukeji.net>
2021-09-09 19:33:21 +0800

    '初步搭建'

---
+++ src/main/java/com/sunyue/course/CourseApplication.java
@@ -0,0 +1,18 @@
+package com.sunyue.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Angelo
+ * @Describe 启动类
+ * @date 2021/09/09
+ */
+@SpringBootApplication
+public class CourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sunyue/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunyue.course;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
