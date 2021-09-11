afc73cdfa4c7b9248099862d8a03a62f4d561dd0
Angelo <ming.bu@weizhukeji.net>
2021-09-11 08:50:38 +0800

    '1、新增gateway子模块，增加gateway依赖，并注册到配置中心。'

---
+++ gateway/src/main/java/com/course/gateway/GatewayApplication.java
@@ -0,0 +1,20 @@
+package com.course.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author Angelo
+ * @Describe 启动类
+ * @date 2021/09/09
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+}
